ff6cf76e0c9057724363bb509b098028974b6330
Megagig <megagigsoftwaresolutions.com>
2024-03-01 02:59:03 +0100

    Install ReactTooltip dependency

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { motion } from 'framer-motion';
+// import ReactTooltip from 'react-tooltip';
+
+import { AppWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
 import './Skills.scss';
 
 const Skills = () => {
   return (
-    <div>
-      <h1>Skills</h1>
-    </div>
+    <>
+      <h2 className="head-text"> Skills & Experience</h2>
+    </>
   );
 };
 
